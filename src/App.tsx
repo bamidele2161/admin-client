717fdd7d90c26301dfa087a94eb4768acf22180e
bamidele2161 <akinyemibamidele2@gmail.com>
2025-09-25 05:35:57 -0700

    Set up admin codebase

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { adminRoutes, authRoutes } from "./routes/routes";
+import DashboardLayout from "./layout/Dashboard";
+import Livechat from "./components/Chatwoot";
+import Guard from "./routes/Guard";
+
+function App() {
+  return (
+    <main className="App">
+      <ToastContainer position="top-right" autoClose={2000} />
+
+      <Routes>
+        {authRoutes.map((route, idx: number) => (
+          <Route key={idx} path={route.path} element={route.element} />
+        ))}
+
+        {adminRoutes.map((route, idx: number) => (
+          <Route
+            key={idx}
+            path={route.path}
+            element={
+              <Guard>
+                <DashboardLayout>{route.element}</DashboardLayout>
+              </Guard>
+            }
+          />
+        ))}
+      </Routes>
+      <Livechat />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,42 @@
+import { type Route } from "../interfaces/Global";
+
+import ProductManagement from "../pages/AdminPages/products/ProductManagement";
+import OrderManagement from "../pages/AdminPages/orders/OrderManagement";
+import UserManagement from "../pages/AdminPages/users/UserManagement";
+import VendorManagement from "../pages/AdminPages/vendors/VendorManagement";
+import ContentManagement from "../pages/AdminPages/content/ContentManagement";
+import AdminDashboard from "../pages/AdminPages/Dashboard";
+import AdminSettings from "../pages/AdminPages/Settings";
+import AdminAuth from "../pages/AdminPages/Auth/Auth";
+
+const authRoutes: Route[] = [
+  {
+    path: "/",
+    name: "Admin SignUp Page",
+    element: <AdminAuth />,
+  },
+];
+
+const adminRoutes: Route[] = [
+  { path: "/admin-dashboard", name: "Home", element: <AdminDashboard /> },
+  {
+    path: "/admin-product-management",
+    name: "Home",
+    element: <ProductManagement />,
+  },
+  {
+    path: "/admin-order-management",
+    name: "Home",
+    element: <OrderManagement />,
+  },
+  {
+    path: "/admin-customer-management",
+    name: "User",
+    element: <UserManagement />,
+  },
+  { path: "/admin-vendor", name: "Home", element: <VendorManagement /> },
+
+  { path: "/admin-content", name: "Discount", element: <ContentManagement /> },
+  { path: "/admin-settings", name: "Settings", element: <AdminSettings /> },
+];
+export { authRoutes, adminRoutes };
